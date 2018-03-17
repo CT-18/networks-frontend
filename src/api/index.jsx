1fc7dee630a87b892bd1d4c5888cb40ee5f02e26
danilkolikov <danilkolikov@gmail.com>
2018-03-17 10:48:13 +0300

    Raspberry link fix

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 export const getStreams = () => Promise.resolve([{
   name: 'Raspberry',
-  url: 'http://10.8.0.3:8080/cam/live.m3u8'
+  url: 'http://10.8.0.3/live.m3u8'
 }, {
   name: 'Parcour in Greece',
   url: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
